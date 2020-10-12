968278465ad75008137a17272d244c6ee2e4eee3
RajeevK <rajeev.sgsits.88@gmail.com>
2020-10-12 11:43:24 +0530

    set locale false for API

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -2,6 +2,7 @@ Apipie.configure do |config|
   config.app_name                = "CompanyHouse"
   config.api_base_url            = "/api"
   config.doc_base_url            = "/apipie"
+  config.translate               = false
   # where is your API defined?
-  config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
+  config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/**/*.rb"
 end
